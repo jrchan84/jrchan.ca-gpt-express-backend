a9213f8f7263a5aac95d1ade7b025a1adfa128ab
jrchan84 <justinrichardchan@gmail.com>
2023-03-15 18:41:31 -0700

    test deployment with local frontend

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
+import cors, { CorsOptions } from 'cors';
 
 dotenv.config();
 
@@ -9,13 +9,16 @@ const port = process.env.PORT;
 
 // Trust App Engine proxy for user IP address connections.
 app.set('trust proxy', true);
-const corsOptions = {
+
+// Specify frontend origin access
+const corsOptions: CorsOptions = {
   origin: ['https://jrchan.ca', 'http://localhost:3000']
 };
 app.use(cors(corsOptions));
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+  const message = {text: 'Hello, World!'};
+  res.json(message);
 });
 
 app.listen(port, () => {
