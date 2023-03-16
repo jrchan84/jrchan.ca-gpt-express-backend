a1ca97528eef92dc4d3fa01066b7af92fb4c4ae8
jrchan84 <justinrichardchan@gmail.com>
2023-03-15 18:22:48 -0700

    Enable CORS for frontend origin

--- index.ts
+++ index.ts
@@ -1,12 +1,18 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Express = express();
+const port = process.env.PORT;
+
 // Trust App Engine proxy for user IP address connections.
 app.set('trust proxy', true);
-const port = process.env.PORT;
+const corsOptions = {
+  origin: ['https://jrchan.ca', 'http://localhost:3000']
+};
+app.use(cors(corsOptions));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
